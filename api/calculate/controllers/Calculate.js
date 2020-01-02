1c9d04af5dd98707971444c66ae9acff474a7e9d
Sergey <klikkn@yandex.ru>
2020-01-02 20:35:14 +0100

    update riutes

---
+++ api/calculate/controllers/Calculate.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+    index: async ctx => {
+        const { prices } = ctx.state.user;
+        const { classIndex, selected, complicated, squares } = ctx.request.body;
+
+        const result = selected.reduce((acc, curr) => {
+            const selectedSquare = squares[curr];
+            console.log(2, selectedSquare)
+            if (selectedSquare === undefined) return acc += 0;
+
+            const priceTable = curr != 'roof' ? complicated[curr] ? prices[1] : prices[0] : prices[2];
+            return acc += priceTable[classIndex][selectedSquare];
+        }, 0)
+
+        ctx.send(result);
+    },
+};
--- api/params/controllers/Params.js
+++ api/params/controllers/Params.js
@@ -5,4 +5,8 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    find: async ctx => {
+        ctx.send(strapi.config.params);
+    },
+};
